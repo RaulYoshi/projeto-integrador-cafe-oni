f817d7fb84241258f5b254b3195cafac762a9852
RaulYoshi <119540267+RaulYoshi@users.noreply.github.com>
2024-12-02 22:53:35 -0300

    troca de nome de banco de dados

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: 'localhost', 
   user: 'root',     
   password: '',     
-  database: 'cafesabor', 
+  database: 'cafeoni', 
 });
 
 connection.connect((err) => {
